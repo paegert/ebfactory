7b546e68025817d0b6609614cfd0f2026f3274a5
paegerm <paegerm>
2013-09-05 18:48:33 +0000

    *** empty log message ***

--- src/makeplc.py
+++ src/makeplc.py
@@ -3,15 +3,19 @@ Created on Jun 18, 2012
 
 @package  ebf
 @author   mpaegert
-@version  \$Revision: 1.3 $
-@date     \$Date: 2012/11/30 20:31:10 $
+@version  \$Revision: 1.4 $
+@date     \$Date: 2013/09/05 18:49:17 $
 
 make phase folded light curves
 
 $Log: makeplc.py,v $
-Revision 1.3  2012/11/30 20:31:10  paegerm
-convert del to nodel option
+Revision 1.4  2013/09/05 18:49:17  paegerm
+*** empty log message ***
 
+switch to nplc (normalized and phased), select stars from vardict view,
+update vars table in dictionary, delete nodel option (we are updating now)
+
+Revision 1.3  2012/11/30 20:31:10  paegerm
 convert del to nodel option
 
 Revision 1.2  2012/09/24 21:35:04  paegerm
@@ -42,24 +46,17 @@ if __name__ == '__main__':
     parser.add_option('--dbconfig', dest='dbconfig', type = 'string', 
                       default='Asas',
                       help='name of database configuration (default = Asas')
-#    parser.add_option('--del', dest='delete', action='store_true', default=True,
-#                      help='per starudi: delete old entries in plc (default = True)')
-    parser.add_option('--nodel', dest='delete', action='store_false', default=True,
-                      help='per staruid: do not delete old entries in plc (default = delete)')
     parser.add_option('--dict', dest='dictname', type='string', 
                       default='asasdict.sqlite',
                       help='dictionary database file')
-    parser.add_option('--rawlc', dest='rawlcname', type='string', 
-                      default='asaslc.sqlite',
-                      help='database file with raw light curves')
-    parser.add_option('--plc', dest='plcname', type='string', 
-                      default='asasplc.sqlite',
-                      help='database file with phased light curves')
+    parser.add_option('--nplc', dest='nplcname', type='string', 
+                      default='asasnplc.sqlite',
+                      help='database file with normalized, phased light curves')
     parser.add_option('--rootdir', dest='rootdir', type='string', 
                       default='./',
                       help='directory for database files (default = ./)')
     parser.add_option('--select', dest='select', type='string', 
-                      default='select * from stars;',
+                      default="select * from vardict;",
                       help='select statement for dictionary (Default: select * from stars;)')
     parser.add_option('--selfile', dest='selfile', type='string', 
                       default=None,
@@ -87,37 +84,34 @@ if __name__ == '__main__':
 
     dictreader = dbr.DbReader(options.rootdir + options.dictname)
     
-    generator = dictreader.traverse(options.select, None, 1000)
-    nrstars   = 0
-    lcreader  = dbr.DbReader(options.rootdir + options.rawlcname)
-    plcwriter = dbw.DbWriter(options.rootdir + options.plcname, dbc.plccols, 
-                             'stars', dbc.plctypes, dbc.plcnulls)
+    generator  = dictreader.traverse(options.select, None, 1000)
+    nrstars    = 0
+    nplcwriter = dbw.DbWriter(options.rootdir + options.nplcname)
     gaps   = []
     failed = 0
+    getlc  = 'select * from stars where staruid = ? order by hjd asc;'
+    cmd = 'update stars set phase = ? where uid = ?;'
     for star in generator:
         nrstars += 1
-        lc = lcreader.getlc(star['uid'], 'stars', 'hjd')
+        lc = nplcwriter.fetchall(getlc, (star['uid'],))
         print 'phasing ', nrstars, star['id']
         period = dbc.getperiod(star)    
         shift  = dbc.getshift(star)        
         if period <= 0.0:
             failed += 1
             continue
-        if options.delete == True:
-            plcwriter.deletebystaruid(star['uid'])
-        (plc, gap) = makephasedlc(lc, star['t0'], star[dbc.t['mag']], 
-                                  period, 0.0)
-        gaps.append ([period, gap, star['uid']])
-        plcwriter.insert(plc)
+        (plc, gap, t0) = phaselc(lc, star['t0'], period, 0.0)
+        gaps.append ([gap, t0, star['uid']])
+        nplcwriter.update(cmd, plc, True)
     
-    plcwriter.commit()
-    plcwriter.close()
+    nplcwriter.commit()
+    nplcwriter.close()
     dictreader.close()
     
     print 'updating dictionary'
-    dictwriter = dbw.DbWriter(options.rootdir + options.dictname, dbc.dictcols)
-    dictwriter.update('update stars set period = ?, plcmaxgap = ? where uid = ?;', 
-                      gaps, True)
+    dictwriter = dbw.DbWriter(options.rootdir + options.dictname)
+    cmd = 'update vars set plcmaxgap = ?, T0 = ? where staruid = ?;'
+    dictwriter.update(cmd, gaps, True)
     dictwriter.close()
     
     print nrstars, 'light curves read in ', watch.stop(), ' seconds'
